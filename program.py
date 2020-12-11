ec600e2ebcb2d1898a5bc270343b99c4be9e006c
Л <klad-18@yandex.ru>
2020-12-11 19:01:59 +0500

    напечатали единицу

--- program.py
+++ program.py
@@ -6,7 +6,8 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
-    my_superfunction
+    my_superfunction()
+    print(1)
 
 if __name__ == '__main__':
     main()
